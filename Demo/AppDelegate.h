8b3b5c68a6c049eff6f26aa861c5c5bd212edc8b
luwenchao <lwc_funnylife@126.com>
2017-08-25 10:50:46 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by cheyipai on 2017/8/25.
+//  Copyright © 2017年 cheyipai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by cheyipai on 2017/8/25.
+//  Copyright © 2017年 cheyipai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
